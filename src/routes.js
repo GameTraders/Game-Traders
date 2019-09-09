823fb82588698cbbd4441ed90bb144f8071ea321
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-09-09 15:28:30 -0600

    Authentication Styling and functionality added

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from "react"
 import {Route, Switch} from "react-router-dom"
 import Authentication from "./Components/Authentication/Authentication.js"
+import Dashboard from "./Components/Dashboard/Dashboard.js"
 
 export default (
     <Switch>
         <Route component = {Authentication}  path = "/" exact/>
+        <Route component = {Dashboard}  path = "/home" exact/>
     </Switch>
 )
