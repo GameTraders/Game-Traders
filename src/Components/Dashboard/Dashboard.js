1677b261291b4e64a4a3ff607b626059c9fc272c
Danny Strachan <dannystrachan@Dannys-MacBook-Pro.local>
2019-09-10 13:31:40 -0600

    Dashboard Styled

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,7 +1,282 @@
-import React from "react"
+import './Dashboard.css'
+import React, {Component} from "react"
 
-export default function Dashboard() {
-return(
-    <h5>Dashboard</h5>
-)
+export default class Dashboard extends Component{
+    state = {
+        checkBox: true,
+        games: [{
+            name: "Halo Infinite",
+            cover: "https://i.redd.it/uk00vkrvfkb11.png",
+            points: 40
+        },{
+          name: "Rocket League",
+          cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+          points: 50
+      },{
+          name: "GTA 5",
+          cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+          points: 45
+      },{
+          name: "Zelda",
+          cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+          points: 60
+      },{
+          name: "Halo Infinite",
+          cover: "https://i.redd.it/uk00vkrvfkb11.png",
+          points: 40
+      },{
+        name: "Rocket League",
+        cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+        points: 50
+    },{
+        name: "GTA 5",
+        cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+        points: 45
+    },{
+        name: "Zelda",
+        cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+        points: 60
+    },{
+      name: "Halo Infinite",
+      cover: "https://i.redd.it/uk00vkrvfkb11.png",
+      points: 40
+  },{
+    name: "Rocket League",
+    cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+    points: 50
+  },{
+    name: "GTA 5",
+    cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+    points: 45
+  },{
+    name: "Zelda",
+    cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+    points: 60
+  },{
+    name: "Halo Infinite",
+    cover: "https://i.redd.it/uk00vkrvfkb11.png",
+    points: 40
+},{
+  name: "Rocket League",
+  cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+  points: 50
+},{
+  name: "GTA 5",
+  cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+  points: 45
+},{
+  name: "Zelda",
+  cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+  points: 60
+},{
+  name: "Halo Infinite",
+  cover: "https://i.redd.it/uk00vkrvfkb11.png",
+  points: 40
+},{
+name: "Rocket League",
+cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+points: 50
+},{
+name: "GTA 5",
+cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+points: 45
+},{
+name: "Zelda",
+cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+points: 60
+},{
+name: "Halo Infinite",
+cover: "https://i.redd.it/uk00vkrvfkb11.png",
+points: 40
+},{
+name: "Rocket League",
+cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+points: 50
+},{
+name: "GTA 5",
+cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+points: 45
+},{
+name: "Zelda",
+cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+points: 60
+},{
+    name: "Halo Infinite",
+    cover: "https://i.redd.it/uk00vkrvfkb11.png",
+    points: 40
+},{
+  name: "Rocket League",
+  cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+  points: 50
+},{
+  name: "GTA 5",
+  cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+  points: 45
+},{
+  name: "Zelda",
+  cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+  points: 60
+},{
+  name: "Halo Infinite",
+  cover: "https://i.redd.it/uk00vkrvfkb11.png",
+  points: 40
+},{
+name: "Rocket League",
+cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+points: 50
+},{
+name: "GTA 5",
+cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+points: 45
+},{
+name: "Zelda",
+cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+points: 60
+},{
+name: "Halo Infinite",
+cover: "https://i.redd.it/uk00vkrvfkb11.png",
+points: 40
+},{
+name: "Rocket League",
+cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+points: 50
+},{
+name: "GTA 5",
+cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+points: 45
+},{
+name: "Zelda",
+cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+points: 60
+},{
+name: "Halo Infinite",
+cover: "https://i.redd.it/uk00vkrvfkb11.png",
+points: 40
+},{
+name: "Rocket League",
+cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+points: 50
+},{
+name: "GTA 5",
+cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+points: 45
+},{
+name: "Zelda",
+cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+points: 60
+},{
+name: "Halo Infinite",
+cover: "https://i.redd.it/uk00vkrvfkb11.png",
+points: 40
+},{
+name: "Rocket League",
+cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+points: 50
+},{
+name: "GTA 5",
+cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+points: 45
+},{
+name: "Zelda",
+cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+points: 60
+},{
+name: "Halo Infinite",
+cover: "https://i.redd.it/uk00vkrvfkb11.png",
+points: 40
+},{
+name: "Rocket League",
+cover: "https://cdn0.spong.com/pack/r/o/rocketleag434170l/_-Rocket-League-Xbox-One-_.jpg",
+points: 50
+},{
+name: "GTA 5",
+cover: "https://www.gtabase.com/igallery/601-700/GTA_V_Cover_XboxOne-652-1920.jpg",
+points: 45
+},{
+name: "Zelda",
+cover: "https://vgboxart.com/boxes/WiiU/75322-zelda-breath-of-the-wild-nintendo-switch.png",
+points: 60
+}]
+    }
+    render() {
+        let miniGames = this.state.games.map((e, i) => {
+            return (
+                <div key={i} className="home-game-mini">
+                        <h4 className="home-mini-name">
+                            {e.name.length > 5 ? `${e.name.substring(0, 6)}...` : `${e.name}` }
+                        </h4>
+                        <h4 className="mini-name-hover">{e.name}</h4>
+                        <div className="home-mini-dispay">
+                            <img className="home-mini-cover-art" alt="" src={e.cover} />
+                            <div className="home-game-mini-points">{e.points}</div>
+                        </div>
+                </div>
+            )
+        })
+        const {checkBox} = this.state
+        return(
+            <div className="Dashboard">
+                <div className="Dashboard_NavBar">
+                        <div className="Dashboard_Logo">
+                            <h1>Game Traders</h1>
+                        </div>
+                        <div className="Dashboard-search-container">
+                            <button onClick={this.Login} className="Authentication_Button">
+                                <h4>Search</h4>
+                            </button>
+                            <div className="Authentication_Username_Container">
+                                <h4>Search</h4>
+                                <input onChange={(e)=> {this.handleChange("username", e.target.value)}} className="Authentication_Input" type="text" placeholder="Username" />
+                            </div>
+                            <div className="check-container"  >
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>M - Rated</p>
+                                </div>
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>XBox 360</p>
+                                </div>
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>XBox One</p>
+                                </div>
+                            </div>
+                            <div className="check-container"  >
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>PlayStation 2</p>
+                                </div>
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>PlayStation 3</p>
+                                </div>
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>PlayStation 4</p>
+                                </div>
+                            </div>
+                            <div className="check-container"  >
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>Wii</p>
+                                </div>
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>Nintendo Switch</p>
+                                </div>
+                                <div className="filter-option">
+                                    <input className="check" checked={checkBox} type="checkbox" onChange={() => this.setState({checkBox: !checkBox})} />
+                                    <p>PlayStation 4</p>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <div className="dashboard-container">
+                    {miniGames}
+                </div>
+            </div>
+        )
+    }
 }
+
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from "react"
 import {Route, Switch} from "react-router-dom"
 import Authentication from "./Components/Authentication/Authentication.js"
 import Dashboard from "./Components/Dashboard/Dashboard.js"
+import Trader from "./Components/Trader/Trader.js"
 
 export default (
     <Switch>
         <Route component = {Authentication}  path = "/" exact/>
         <Route component = {Dashboard}  path = "/home" exact/>
+        <Route component = {Trader}  path = "/trader" exact/>
     </Switch>
 )
