1f14adeec606ac24c014221cbe48bc7cca7d3698
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-09-10 12:13:45 -0600

    update

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react"
+import axios from "axios"
+
+export default class Dashboard extends Component {
+    state = {
+        gamename: ""
+    }
+
+    handleChange(key, e) {
+        this.setState({
+            [key]: e
+        })
+    }
+    getname = async () => {
+        const name = this.state.gamename
+        console.log(name)
+        const results = await axios.post("/api/games", {name})
+        console.log(results)
+    }
+    render() {
+
+        return (
+            <div>
+                <h5>Dashboard</h5>
+                <input onChange={(e)=>{this.handleChange("gamename", e.target.value)}} type="text" placeholder="Search by Game Name"/>
+                <button onClick={this.getname}>Get by name</button>
+            </div>
+        )
+    }
+}
