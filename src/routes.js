20da12d3f5a3f1593d95d30973bddbde273aba32
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-09-11 14:17:39 -0600

    Trades Styling Implimented

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Dashboard from "./Components/Dashboard/Dashboard.js"
 import Search from "./Components/Search/Search"
 import UserProfile from './Components/Profiles/UserProfile'
 import Trader from './Components/Trader/Trader'
+import Trades from './Components/Trades/Trades'
 
 export default (
     <Switch>
@@ -14,5 +15,6 @@ export default (
         <Route component = {Search}  path = "/search" />
         <Route component = {UserProfile} path = '/userProfile/:user_id'/>
         <Route component = {Trader} path = '/trader'/>
+        <Route component = {Trades} path = '/trades'/>
     </Switch>
 )
